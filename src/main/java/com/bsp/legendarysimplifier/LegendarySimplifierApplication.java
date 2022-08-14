5a705f6a327c3397634c37b74fbd5d2ab87fe542
Buddhika Semasinghe <buddhika.semasinghe@gmail.com>
2022-08-14 17:21:59 +1000

    using sping initilizer

---
+++ src/main/java/com/bsp/legendarysimplifier/LegendarySimplifierApplication.java
@@ -0,0 +1,13 @@
+package com.bsp.legendarysimplifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegendarySimplifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegendarySimplifierApplication.class, args);
+	}
+
+}
